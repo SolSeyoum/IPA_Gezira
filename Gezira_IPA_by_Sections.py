75cf9e650413fa1d8eed1553057f1370e7b61821
Solomon <sd.seyoum@yahoo.com>
2025-03-05 20:41:03 +0100

    Update Gezira_IPA_by_Sections.py

--- Gezira_IPA_by_Sections.py
+++ Gezira_IPA_by_Sections.py
@@ -9,6 +9,12 @@ import json
 from shapely.geometry import Polygon, mapping
 from shapely.ops import unary_union
 from PIL import Image
+
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 #######################
 # Page configuration
 st.set_page_config(
