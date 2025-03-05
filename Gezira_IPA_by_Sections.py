0a002e25280f5aa3015e75c55baf19180eea3186
Solomon <sd.seyoum@yahoo.com>
2025-03-05 18:20:56 +0100

    Add files via upload
    first version of the Gezira irrigation scheme IPA app

---
+++ Gezira_IPA_by_Sections.py
@@ -0,0 +1,337 @@
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+from shapely.geometry import Polygon, mapping
+from shapely.ops import unary_union
+from PIL import Image
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="Gezira Irrigation Scheme Irrigation Performance Indicators by Sections Dashboard",
+    page_icon="📈🌿",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #1c1b1b;
+    text-align: center;
+    padding: 2px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+            
+img[data-testid="stLogo"] {
+            height: 4.5rem;
+}
+</style>
+""", unsafe_allow_html=True)
+
+#######################
+# Load data
+# dfm = pd.read_csv(r'data/Mwea_IPA_stat_by_blocks.csv')
+with open(r'data\Gezira_IR.json') as response:
+    geo = json.load(response)
+
+# dfm.columns = [x.replace('_', ' ') for x in dfm.columns]
+logo_wide = r'data\logo_wide.png'
+logo_small = r'data\logo_small.png'
+
+IPA_description = {
+    "beneficial fraction": ":blue[Beneficial fraction (BF)] is the ratio of the water that is consumed as transpiration\
+         compared to overall field water consumption (ETa). ${\\footnotesize BF = T_a/ET_a}$. \
+         It is a measure of the efficiency of on farm water and agronomic practices in use of water for crop growth.",
+    "crop water deficit": ":blue[crop water deficit (CWD)] is measure of adequacy and calculated as the ration of seasonal\
+        evapotranspiration to potential or reference evapotranspiration ${\\footnotesize CWD= ET_a/ET_p}$",
+    "relative water deficit": ":blue[relative water deficit (RWD)] is also a measure of adequacy which is 1 minus crop water\
+          deficit ${\\footnotesize RWD= 1-ET_a/ET_p}$",
+    "total seasonal biomass production": ":blue[total seasonal biomass production (TBP)] is total biomass produced in tons. \
+        ${\\footnotesize TBP = (NPP * 22.222) / 1000}$",
+    "seasonal yield": ":blue[seasonal yield] is the yield in a season which is crop specific and calculated using \
+        the TBP and yield factors such as moisture content, harvest index, light use efficiency correction \
+            factor and above ground over total biomass production ratio (AOT) \
+                ${\\footnotesize Yiled = TBP*HI*AOT*f_c/(1-MC)}$",
+    "crop water productivity": ":blue[crop water productivity (CWP)] is the seasonal yield per the amount of water \
+        consumed in ${kg/m^3}$"
+}
+
+crop_calendar = {'wheats': 'November to March', 'sorgums':'June to December', 'cottons':'June to March'}
+
+# @st.cache_data(ttl=300)
+def load_image(image_name: str) -> Image:
+    """Displays an image.
+
+    Parameters
+    ----------
+    image_name : str
+        Local path of the image.
+
+    Returns
+    -------
+    Image
+        Image to be displayed.
+    """
+    return Image.open(image_name)
+#######################
+# Sidebar
+with st.sidebar:
+
+    st.logo(load_image(logo_wide), size="large", link='https://www.un-ihe.org/', icon_image=load_image(logo_small))
+    st.title('Gezira Irrigation Performance Indicators')
+
+    stat_dict = {'Average':'mean', 'Minimum': 'min', 'Maximum':'max', 'Median':'meadian','Standard deviation':'std'}
+
+    crop_lst = ['wheats', 'sorgums', 'cottons']
+    selected_crop = st.selectbox('Select crop type', crop_lst)
+    dfm = pd.read_csv(fr'data/Gezira_IPA_statistic_{selected_crop}.csv')
+
+    season_list = list(dfm.season.unique())[::-1]
+    ll = list(dfm.columns.unique())[3:][::-1]
+    indicator_lst = [' '.join(l.split('_')[1:]) for l in ll]
+    
+    selected_season = st.selectbox('Select a season', season_list)
+    st.write(f'The :blue[season] for :blue[{selected_crop}] runs from months of :blue[{crop_calendar[selected_crop]}].')
+    
+    indicator = st.selectbox('Select an indicator', set(indicator_lst))
+
+    stats_list = list(stat_dict.keys())
+    selected_stat = st.selectbox('Select a statistics', stats_list) 
+    # selected_indicator = f'{indicator.replace(' ', '_')}'
+    st.write(f'{IPA_description[indicator]}')
+   
+    selected_indicator = f'{stat_dict[selected_stat]}_{indicator.replace(' ', '_')}'
+    df_selected = dfm[dfm.season == selected_season][['section_name', selected_indicator]]
+    df_selected_sorted = df_selected.sort_values(by=selected_indicator, ascending=False).reset_index()
+    del df_selected_sorted['index']
+
+#######################
+
+# Plots
+
+def indicator_title(indicator, stat_dict):
+    # stat_dict = {'std':'Standard deviation', 'min':'Minimum', 'max':'Maximum', 'mean':'Average'}
+    lst = indicator.split('_')
+    t1 = ' '.join(lst[1:])
+    t2 = f"{[k for k, v in stat_dict.items() if v == lst[0]][0]} {t1}" 
+    return t1,t2
+
+
+# Choropleth map
+def make_Choroplethmapbox(geo, indicator, stat_dict, df, season, unit, crop ):
+    
+    ylable, text = indicator_title(indicator, stat_dict)
+    print('plots', df.columns[0])
+    col_name = df.columns[1]
+    df['indicator'] = ylable
+    fig = px.choropleth_mapbox(df,  # dataframe to plothangi veri seti
+                                geojson=geo,  # the geolocation
+                                locations=df['section_name'],
+                                featureidkey="properties.Irr_Sub_Di",
+                                color=col_name,  
+                                color_continuous_scale="Viridis",  #
+                                range_color=(df[col_name].min(),
+                                            df[col_name].max()),
+                                
+                                center={"lat": 14.429, "lon":  33.01},
+                                mapbox_style="carto-darkmatter",  # mapbox style
+                                template='plotly_dark',
+                                zoom=7,  # zoom level
+                                opacity=0.9,  # opacity
+                                custom_data=[df['section_name'],
+                                            df[col_name], 
+                                            # df['block'],
+                                            df['indicator']] ,
+                                width=600, height=400,
+                                )
+    fig.update_layout(title=f"Map of {text} for season {season}: {crop}",
+                        # title_x=0.5  # Title position
+                        )
+    # colrbar configuration
+    fig.update_layout(
+                        # coloraxis_colorbar_x=0.95,
+                        coloraxis_colorbar_title=f'{ylable} [{unit}]', 
+                        coloraxis_colorbar_title_side="right",
+                        coloraxis_colorbar_thickness=15,
+                    )
+    # hver template
+    hovertemp = '<i style="color:white;">Section:</i><b> %{customdata[0]}</b><br>'
+    hovertemp += '<i>Block:</i><b> %{customdata[2]}</b><br>'
+    hovertemp += "%{customdata[2]}: %{customdata[1]:,.2f}<br>"
+    fig.update_traces(hovertemplate=hovertemp)
+    fig.update_layout(margin={"r":0, "l":0, "b":0})
+    fig.show()
+    return fig
+
+# histogram plot
+def make_alt_chart(df, indicator, stat_dict, crop):
+    ylable, text = indicator_title(indicator, stat_dict)
+    w = ylable.split()
+    if(len(w)%2):
+        w.append ("")
+    ylable = [' '.join((w[2*i], w[2*i+1]))  for i in range(len(w)//2)]
+    yrs = len(df.season.unique())
+    # ylable1 = re.sub(r'(\s\S*?)\s', r'\1\n ',ylable)
+    title = alt.TitleParams(text = f'{text} per section for seasons'
+                                   f' {df.season.iloc[0]} to {df.season.iloc[-1]}: {crop}',
+                                        fontSize = 16, fontWeight = 'bold', subtitleFontWeight = 'bold'
+                                        )
+
+    chart = alt.Chart(df, title=title).mark_bar().encode(
+        x=alt.X('section_name:N', axis=None),
+        y=alt.Y(f'{indicator}:Q', title=ylable),
+        color='section_name:N',
+        column='season:N'
+    ).properties(width=600/yrs, height=80).configure_legend(
+        orient='bottom',
+        columns=6,
+    )
+    return chart
+
+def format_number(num):
+    return f"{num:.2f}"
+
+# Calculation year-over-year difference in metrix
+def calculate_indicator_difference(input_df, indicator, input_season):
+  selected_season_data = input_df[input_df['season'] == input_season].reset_index()
+  prev_season  = f'{int(input_season.split('-')[0])-1}-{int(input_season.split('-')[0])}'
+  previous_year_data = input_df[input_df['season'] == prev_season].reset_index()
+  selected_season_data['indicator_difference'] = selected_season_data[indicator].sub(previous_year_data[indicator], fill_value=0)
+  return pd.concat([selected_season_data['section_name'], selected_season_data[indicator], selected_season_data.indicator_difference], axis=1).sort_values(by="indicator_difference", ascending=False)
+
+
+def history_df(df1, df2, idx_col):
+    df1 = df1.fillna(0)
+    df2 = df2.fillna(0)
+    d2 = df1.pivot(index=idx_col, columns='season', values=selected_indicator).reset_index()
+    d3 = df2.groupby(idx_col).agg({selected_indicator:'mean'}).reset_index()
+    d4 = d3.merge(d2, on=idx_col, how = 'inner')
+    d4[d4.columns[2:]]= d4[d4.columns[2:]].round(2)
+    d4['history'] = d4[d4.columns[2:]].values.tolist()
+    d4 = d4.drop(columns = d4.columns[2:-1])
+    return d4.round(2)
+
+#######################
+# Dashboard Main Panel
+col = st.columns((4, 1.0, 2.5), gap='medium')
+
+with col[1]:
+    st.markdown('###### Gains/Losses from previous year')
+
+    input_df = dfm[['season','section_name', selected_indicator]]
+    df_indicator_difference_sorted = calculate_indicator_difference(input_df, selected_indicator, selected_season)
+
+    if selected_season > dfm['season'].min():
+        first_section_name = df_indicator_difference_sorted['section_name'].iloc[0]
+        first_section_name_indicator = format_number(df_indicator_difference_sorted[selected_indicator].iloc[0])
+        first_section_name_delta = format_number(df_indicator_difference_sorted.indicator_difference.iloc[0])
+    else:
+        first_section_name = '-'
+        first_section_name_indicator = '-'
+        first_section_name_delta = ''
+    st.metric(label=first_section_name, value=first_section_name_indicator, delta=first_section_name_delta)
+
+    if selected_season > dfm['season'].min():
+        last_first_section_name = df_indicator_difference_sorted['section_name'].iloc[-1]
+        last_section_name_indicator = format_number(df_indicator_difference_sorted[selected_indicator].iloc[-1])   
+        last_section_name_delta = format_number(df_indicator_difference_sorted.indicator_difference.iloc[-1])   
+    else:
+        last_first_section_name = '-'
+        last_section_name_indicator = '-'
+        last_section_name_delta = ''
+    st.metric(label=last_first_section_name, value=last_section_name_indicator, delta=last_section_name_delta)
+
+
+with col[0]:
+    st.markdown('###### Indicator Map and Chart')
+    
+    units = {'beneficial fraction':'-', 'crop water deficit': '-',
+       'relative water deficit': '-', 'total seasonal biomass production': 'ton',
+       'seasonal yield': 'ton/ha', 'crop water productivity': 'kg/m<sup>3</sup>'}
+    
+    choropleth = make_Choroplethmapbox(geo, selected_indicator, stat_dict, df_selected_sorted, selected_season, units[indicator], selected_crop)
+    st.plotly_chart(choropleth, use_container_width=True)
+
+    st.write("")
+    dfm_var = dfm[['season', selected_indicator,'section_name']]
+    
+    bar_chart = make_alt_chart(dfm_var, selected_indicator, stat_dict, selected_crop)
+    st.altair_chart(bar_chart, use_container_width=False)
+    
+
+with col[2]:
+    st.markdown('###### Indictaor ranked')
+  
+    ylable, text = indicator_title(selected_indicator, stat_dict)
+    st.write(ylable)
+
+    df = history_df(dfm_var, df_selected_sorted, 'section_name')
+    ymin = df.history.apply(lambda x: min(x)).min()
+    ymax = df.history.apply(lambda x: max(x)).max()
+    st.dataframe(df,
+                 column_config={
+                    "section_name": st.column_config.TextColumn(
+                        "Name",
+                    ),
+                    selected_indicator: st.column_config.NumberColumn(
+                    f"⭐{selected_season}",
+                    help=f"value of the indicator for the selected season ({selected_season})",
+                    ),
+                    "history": st.column_config.LineChartColumn(
+                        f"values since the {dfm.season.min()} season", y_min=ymin, y_max=ymax,
+                     help=f"value of the indicator for the season since {dfm.season.min()}",   
+                    )
+                     },
+                       hide_index=True,
+                     
+                 )
+       
+    with st.expander('About', expanded=True):
+        st.write('''
+            - Irrigation Performance Indicators are calcukated from data: [FAO WaPOR data](https://www.fao.org/in-action/remote-sensing-for-water-productivity/wapor-data/en).
+            - :orange[**Gains/Losses**]: sections with high and low increase in the selected indicator from the previous year.
+            - :orange[**Indicator ranked**]: shows the ranking of the section based on the selected indicator.
+            ''')
---
+++ pages/Gezira_IPA_raster_viewer.py
@@ -0,0 +1,317 @@
+import streamlit as st
+import rasterio
+import xarray as xr
+import pandas as pd
+import altair as alt
+import folium
+from folium.raster_layers import ImageOverlay, TileLayer
+from branca.colormap import LinearColormap
+from streamlit_folium import folium_static
+import numpy as np
+from pyproj import Transformer
+import re
+import io
+import base64
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+#===================================
+# Page configuration
+st.set_page_config(
+    page_title="Gezira Irrigation Scheme Irrigation Performance Indicators by Sections Dashboard",
+    page_icon="📈🌿",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #1c1b1b;
+    text-align: center;
+    padding: 2px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+            
+img[data-testid="stLogo"] {
+            height: 4.5rem;
+}
+</style>
+""", unsafe_allow_html=True)
+#========================================
+
+# Access wapor via google bucket 
+# dfm.columns = [x.replace('_', ' ') for x in dfm.columns]
+logo_wide = r'data\logo_wide.png'
+logo_small = r'data\logo_small.png'
+ipa_ds_path = r'data\nc\IPA_results_Gezira.nc'
+
+
+@st.cache_data(ttl=300)
+def load_image(image_name: str) -> Image:
+    """Displays an image.
+
+    Parameters
+    ----------
+    image_name : str
+        Local path of the image.
+
+    Returns
+    -------
+    Image
+        Image to be displayed.
+    """
+    return Image.open(image_name)
+
+#######################
+
+IPA_description = {
+    "beneficial fraction": ":blue[Beneficial fraction (BF)] is the ratio of the water that is consumed as transpiration\
+         compared to overall field water consumption (ETa). ${\\footnotesize BF = T_a/ET_a}$. \
+         It is a measure of the efficiency of on farm water and agronomic practices in use of water for crop growth.",
+    "crop water deficit": ":blue[crop water deficit (CWD)] is measure of adequacy and calculated as the ration of seasonal\
+        evapotranspiration to potential or reference evapotranspiration ${\\footnotesize CWD= ET_a/ET_p}$",
+    "relative water deficit": ":blue[relative water deficit (RWD)] is also a measure of adequacy which is 1 minus crop water\
+          deficit ${\\footnotesize RWD= 1-ET_a/ET_p}$",
+    "total seasonal biomass production": ":blue[total seasonal biomass production (TBP)] is total biomass produced in tons. \
+        ${\\footnotesize TBP = (NPP * 22.222) / 1000}$",
+    "seasonal yield": ":blue[seasonal yield] is the yield in a season which is crop specific and calculated using \
+        the TBP and yield factors such as moisture content, harvest index, light use efficiency correction \
+            factor and above ground over total biomass production ratio (AOT) \
+                ${\\footnotesize Yiled = TBP*HI*AOT*f_c/(1-MC)}$",
+    "crop water productivity": ":blue[crop water productivity (CWP)] is the seasonal yield per the amount of water \
+        consumed in ${kg/m^3}$"
+}
+
+units = {'beneficial fraction':'-', 'crop water deficit': '-',
+       'relative water deficit': '-', 'total seasonal biomass production': 'ton',
+       'seasonal yield': 'ton/ha', 'crop water productivity': 'kg/m<sup>3</sup>'}
+
+crop_calendar = {'wheats': 'November to March', 'sorgums':'June to December', 'cottons':'June to March'}
+# Sidebar
+with st.sidebar:
+
+    st.logo(load_image(logo_wide), size="large", link='https://www.un-ihe.org/', icon_image=load_image(logo_small))
+    st.title('Gezira Irrigation Performance Indicators')
+
+    dfm = pd.read_csv(fr'data/Gezira_IPA_statistic_sorgums.csv')
+    season_list = list(dfm.season.unique())[::-1]    
+    selected_season = st.selectbox('Select a season', season_list)
+    season_end_yr = selected_season.split('-')[1]    
+
+
+    ll = list(dfm.columns.unique())[3:][::-1]
+
+    indicator_lst = [' '.join(l.split('_')[1:]) for l in ll]
+     
+    indicator = st.selectbox('Select an indicator', set(indicator_lst))
+    data_array_name = indicator
+  
+
+@st.cache_data
+def read_raster_local(raster_path):
+    with rasterio.open(raster_path) as dataset:  
+        # dataset.nodata = -9999 
+        data = dataset.read(1)
+        transform = dataset.transform
+        crs = dataset.crs
+        nodata = -9999 #dataset.nodata
+        bounds = dataset.bounds
+    
+    return data, transform, crs, nodata, bounds
+
+@st.cache_data
+def read_dataset(ds_path):
+    with xr.open_dataset(ds_path) as dataset:  
+        # data = dataset.beneficial_fraction[0].values
+        transform = dataset.rio.transform()
+        crs = dataset.rio.crs
+        nodata = -9999 #dataset.nodata
+        bd = dataset.rio.bounds()
+        bounds = rasterio.coords.BoundingBox(bd[0], bd[1], bd[2], bd[3])
+    
+    return dataset, transform, crs, nodata, bounds
+
+
+def display_image(data, transform, crs, variable, nodata, bounds):
+    try:
+        # Apply scale factor
+        scale_factor = 1#0.001
+        data = np.nan_to_num(data, nan=-9999)
+        data = np.flip(data,0)
+        data = data.astype(float)
+        
+        # Create a custom colormap
+        colors = ['red', 'yellow', 'green']
+        n_bins = 100
+        cmap = mcolors.LinearSegmentedColormap.from_list("custom", colors, N=n_bins)
+        
+        # Normalize data
+        valid_data = data[data != nodata]
+        if valid_data.size == 0:
+            st.error("No valid data available for the selected image.")
+            return
+        
+        # vmin, vmax =  valid_data.min(), valid_data.max()#np.percentile(valid_data, [2, 98])
+        # vmin = max(vmin, 0.001)
+        vmin, vmax =  valid_data.min(), valid_data.max()#np.percentile(valid_data, [2, 98])
+        # vmin = max(vmin, 0.001)
+        norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+        
+        # Apply colormap to data
+        colored_data = cmap(norm(data))
+        
+        # Set alpha channel to 0 for no-data values
+        colored_data[..., 3] = np.where(data == nodata, 0, 0.7)
+        
+        
+        # Convert to PIL Image
+        img = Image.fromarray((colored_data * 255).astype(np.uint8))
+        
+        # Save image to bytes
+        img_bytes = io.BytesIO()
+        img.save(img_bytes, format='PNG')
+        img_bytes.seek(0)
+        
+        # Encode image to base64
+        img_base64 = base64.b64encode(img_bytes.getvalue()).decode()
+    
+        # Transform bounds to EPSG:4326 (WGS84)
+        transformer = Transformer.from_crs(crs, "EPSG:4326", always_xy=True)
+        left, bottom = transformer.transform(bounds.left, bounds.bottom)
+        right, top = transformer.transform(bounds.right, bounds.top)
+
+        # Create a folium map
+        m = folium.Map(location=[(bottom + top) / 2, (left + right) / 2], zoom_start=8.3, control_scale=True)
+        
+        # Add aerial background
+        TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Aerial Imagery',
+            overlay=False,
+            control=True
+        ).add_to(m)
+        
+        # Add the image overlay
+        ImageOverlay(
+            image=f"data:image/png;base64,{img_base64}",
+            bounds=[[bottom, left], [top, right]],
+            opacity=1.0,
+            name=variable
+        ).add_to(m)
+    
+        # Add the color map legend
+        colormap = LinearColormap(colors=colors, vmin=vmin, vmax=vmax)
+        colormap.add_to(m)
+        colormap.caption = f"{variable} Values"
+
+        
+        # Add layer control and fullscreen option
+        folium.LayerControl().add_to(m)
+        folium.plugins.Fullscreen().add_to(m)
+        
+        # Display the map
+        folium_static(m)
+        
+        # Display additional information
+        # st.write(f"{variable} values range from {vmin:.2f} to {vmax:.2f} {units[indicator]}")
+        
+    except Exception as e:
+        st.error(f"An error occurred while processing the data: {str(e)}")
+        st.write("Debug information:")
+        st.write(f"Data shape: {data.shape}")
+        st.write(f"Transform: {transform}")
+        st.write(f"CRS: {crs}")
+        st.write(f"Bounds: {bounds}")
+# @st.cache_data
+def get_stats(_data):
+      # Compute spatial statistics
+    _data = _data.where(_data>0, np.nan)
+    stats = {
+        'Minimum': _data.min(dim=['latitude', 'longitude']),
+        'Maximum': _data.max(dim=['latitude', 'longitude']),
+        'Mean': _data.mean(dim=['latitude', 'longitude']),
+        'Median': _data.median(dim=['latitude', 'longitude']),
+        'St. deviation': _data.std(dim=['latitude', 'longitude']),
+        "25% quantile": _data.quantile(0.25, dim=['latitude', 'longitude'], method='linear')
+                        .drop_vars('quantile'),
+        "75% quantile": _data.quantile(0.75, dim=['latitude', 'longitude'], method='linear')
+                        .drop_vars('quantile'),
+    }
+
+    # pd.DataFrame.from_dict(d)
+    df_stat = pd.DataFrame.from_dict({k: v.values.item() for k, v in stats.items()}, 
+                                    orient='index', columns = ['Values']).round(2)
+    df_stat.index.names = ['Stats']
+    return df_stat
+
+def main():
+
+    try:
+        variable = indicator.replace(' ', '_')
+        # st.markdown(f"#### {indicator} [{units[indicator]}]")
+        slected_time = f'{season_end_yr}-03-31'
+        ds, transform, crs, nodata, bounds = read_dataset(ipa_ds_path)
+        data =  ds.sel(time=slected_time)[variable]
+        
+        df_stats = get_stats(data)
+    except Exception as e:
+        st.error(f"An error occurred while processing the data: {str(e)}")
+
+    # st.title("### Gezira IPA RAster Viewer")
+    col = st.columns((6.0, 2.0), gap='medium')
+    with col[0]:
+        st.markdown(f"#### Gezira IPA Raster Viewer - :blue[{indicator} [{units[indicator]}]] for :blue[{selected_season}] season")
+        with st.spinner("Loading and processing data..."):
+            display_image(data, transform, crs, variable, nodata, bounds)
+        
+
+    with col[1]:
+        st.write('')
+        st.markdown(f"##### :blue[Stats of {indicator} [{units[indicator]}]]")
+        st.dataframe(df_stats, use_container_width=True)
+        # st.markdown(df_stats.to_html(escape=False),unsafe_allow_html=True)
+                        
+            
+if __name__ == "__main__":
+    main()
