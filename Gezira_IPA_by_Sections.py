879ff3cac029a5f52ca33dcef304a880f8e96988
Solomon <sd.seyoum@yahoo.com>
2025-03-05 20:44:23 +0100

    Update Gezira_IPA_by_Sections.py

--- Gezira_IPA_by_Sections.py
+++ Gezira_IPA_by_Sections.py
@@ -10,9 +10,11 @@ from shapely.geometry import Polygon, mapping
 from shapely.ops import unary_union
 from PIL import Image
 
+# Add custom CSS to hide the GitHub icon
 hide_github_icon = """
-
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+#GithubIcon {
+  visibility: hidden;
+}
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 #######################
